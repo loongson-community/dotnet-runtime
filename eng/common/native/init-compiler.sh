6b8862f61cc5a083901f0efea0013e210106899c
WANG Xuerui <git@xen0n.name>
2024-09-03 18:55:03 +0800

    disable compiler hack for now

--- eng/common/native/init-compiler.sh
+++ eng/common/native/init-compiler.sh
@@ -143,9 +143,9 @@ fi
 
 SCAN_BUILD_COMMAND="$(command -v "scan-build$desired_version" 2> /dev/null)"
 
-if [ $build_arch = loongarch64 ]; then
-    CC="loongarch64-unknown-linux-gnu-gcc"
-    CXX="loongarch64-unknown-linux-gnu-g++"
-fi
+#if [ $build_arch = loongarch64 ]; then
+#    CC="loongarch64-unknown-linux-gnu-gcc"
+#    CXX="loongarch64-unknown-linux-gnu-g++"
+#fi
 
 export CC CXX LDFLAGS SCAN_BUILD_COMMAND
